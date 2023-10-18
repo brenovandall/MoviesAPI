d23f94d0d19d456001f8fb4ff383e017dd71c1f9
Breno Van Dall <vbreno335@gmail.com>
2023-10-18 13:30:42 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesAPI/Controllers/MovieController.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    List<Movie> movies = new List<Movie>();
+
+    [HttpPost]
+    public void AddMovieOnTheList(Movie movie)
+    {
+        movies.Add(movie);
+    }
+}
---
+++ FilmesAPI/Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FilmesAPI.Models;
+
+public class Movie
+{
+
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Genre { get; set; }
+    public int Duration_min { get; set; }
+
+}
