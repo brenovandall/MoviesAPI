93f701a2fb6617cd39cb37af2880d5b0688f9d70
breno van dall <vbreno335@gmail.com>
2023-10-19 13:23:01 -0300

    changing some methods and using required functions

--- FilmesAPI/Controllers/MovieController.cs
+++ FilmesAPI/Controllers/MovieController.cs
@@ -3,15 +3,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
 
+// using to start api controller -- >
 [ApiController]
+// every class that has "controller" in the class name ([MovieController] in this case) -- >
 [Route("[controller]")]
+// ControllBase is an inheritance that needs to be declared in a controller class
 public class MovieController : ControllerBase
 {
     List<Movie> movies = new List<Movie>();
 
+    // declaring the api method <get, post, ...>
     [HttpPost]
-    public void AddMovieOnTheList(Movie movie)
+    public void AddMovieOnTheList([FromBody] Movie movie)
     {
+        // using function " [FromBody] " because the paramether will be declared to the body requisition fields -- >
         movies.Add(movie);
     }
 }
--- FilmesAPI/Models/Movie.cs
+++ FilmesAPI/Models/Movie.cs
@@ -1,13 +1,23 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
 
 namespace FilmesAPI.Models;
 
 public class Movie
 {
-
-    public string Id { get; set; }
+    // i can use conditionals at instances that i have in my class -- >
+    // required string, if empty, the message error will appears -- >
+    [Required(ErrorMessage = "You need to type the title of the movie")]
     public string Title { get; set; }
+
+    [Required(ErrorMessage = "You need to type the genre of the movie")]
+    // in this case, i can use conditional function that i can set a max string length -- >
+    [MaxLength(15, ErrorMessage = "String characters exceeded")]
     public string Genre { get; set; }
+
+    [Required(ErrorMessage = "The duration of the movie needs to be informated")]
+    // in this case, i can use conditional to the min and max range field -- >
+    [Range(70, 250, ErrorMessage = "The duration exceeded the time limit")]
     public int Duration_min { get; set; }
 
 }
