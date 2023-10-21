edb5cdf0745b251fbc9cd40d1ec6bb939397679f
brenovandall <vbreno335@gmail.com>
2023-10-21 00:36:27 -0300

    using iActionResult interface and getting more performance

--- FilmesAPI/Controllers/MovieController.cs
+++ FilmesAPI/Controllers/MovieController.cs
@@ -10,33 +10,42 @@ namespace FilmesAPI.Controllers;
 // ControllBase is an inheritance that needs to be declared in a controller class
 public class MovieController : ControllerBase
 {
-    List<Movie> movies = new List<Movie>();
-    private int index = 0;
+    private static List<Movie> movies = new List<Movie>();
+    private static int index = 0;
 
     // declaring the api method <get, post, ...>
     [HttpPost]
-    public void AddMovieOnTheList([FromBody] Movie movie)
+    public IActionResult AddMovieOnTheList([FromBody] Movie movie)
     {
         movie.Id = index++;
         // using function " [FromBody] " because the paramether will be declared to the body requisition fields -- >
         movies.Add(movie);
-
+        //Console.WriteLine(movie.Id);
+        //Console.WriteLine(movie.Title);
+        //Console.WriteLine(movie.Genre);
+        //Console.WriteLine(movie.Duration_min);
+        // action that create the movie and returns the location of armazenated data -- >
+        return CreatedAtAction(nameof(SortMoviesById), new { id = movie.Id }, movie);
     }
 
     // declaring the api method <get, post, ...>
     [HttpGet]
-    public IEnumerable<Movie> GetMoviesList() // return all movies that were send with post method
+    public IEnumerable<Movie> GetMoviesList([FromQuery] int skip = 0, [FromQuery] int take = 10) // return all movies that were send with post method
     {
         // in this case, using IEnumerable cause List has inheritance from this, so i can use IEnumarable for all classes that inherit the interface
-        return movies;
+        return movies.Skip(skip).Take(take);
     }
 
 
-    [HttpGet("{index}")]
-    public void SortMoviesById(int id)
+    [HttpGet("{id}")]
+    public IActionResult SortMoviesById(int id)
     {
-        // using linq to do a conditional that sort movie by the id informated in get method, the id is the paramether of this method
-        movies.FirstOrDefault(movies => movies.Id == id);
+        // IActionResult because the interface returns the action that was executed
+        var moviesVar = movies.FirstOrDefault(m => m.Id == id);
+        // NotFound ==> 404 NotFound Error
+        if (moviesVar == null) return NotFound();
+        // OK ==> 200 Ok Success
+        return Ok(moviesVar);
     }
 
 }
